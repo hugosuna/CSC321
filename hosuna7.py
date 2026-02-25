db2b368e0464e205a5e57a889577077cf1ad5fba
HO <hosuna2@toromail.csudh.edu>
2026-02-25 02:07:12 +0000

    CSC-321 Lab 7

---
+++ hosuna7.py
@@ -0,0 +1,30 @@
+#Author: Hugo Osuna
+#Date: 02-24-26
+#Program Description:
+#This is a simple Python program that tests the implicit declaration for variables in Python
+#as opposed to the explicit declaration we are used to in C or Java. 
+
+x = 5
+
+y = 6.0
+
+z = "Hello"
+
+if x == 4:
+    print("Variable x is equal to 4")
+else:
+    print("Variable x is either greater than or less than 4")
+
+if y >= 6.7:
+    print("Variable y is either greater than or equal to 6.7")
+else:
+    print("Variable y is less than 6.7")
+
+if z == "Goodbye":
+    print("Goodbye and goonight!")
+else:
+    print("Hello and goodmorning!")
+
+
+    
+
